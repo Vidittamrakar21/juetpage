7c69b3f435ef5e559c0beece24de7ab32964f79f
Vidit <vidit.tamrakar16@gmail.com>
2023-09-30 10:45:06 +0530

    30u

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 let but = document.getElementById('but');
+let dept = document.getElementById('dept');
+let lrc = document.getElementById('lrc');
+let fac = document.getElementById('fac');
+let about = document.getElementById('about');
+let contact = document.getElementById('contact');
+
 
 but.addEventListener('click', ()=> {
     window.location = "https://juet.ac.in/TandP/Placement2023.php"
+})
+
+dept.addEventListener('click', ()=> {
+    window.location = "/dept.html"
+})
+
+lrc.addEventListener('click', ()=> {
+    window.location = "/lrc.html"
+})
+
+fac.addEventListener('click', ()=> {
+    window.location = "/faculty.html"
+})
+
+about.addEventListener('click', ()=> {
+    window.location = "/about.html"
+})
+contact.addEventListener('click', ()=> {
+    window.location = "/contact.html"
 })
